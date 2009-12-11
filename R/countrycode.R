c66aed3a3e5cc7b37a3eaad1b7b1029fbad9a8ed
Vincent Arel-Bundock <varel@umich.edu>
2009-12-11 00:00:00 +0000

    version 0.3

--- R/countrycode.R
+++ R/countrycode.R
@@ -1,29 +1,27 @@
-`countrycode` <-
-function(SOURCEVAR, ORIGIN, DESTINATION){
-TARGET <- NA
-if (ORIGIN == "country.name"){
-symmetric_vectors <- cbind(countrycode_data[DESTINATION], countrycode_data$regex)
-symmetric_vectors <- symmetric_vectors[which (is.na(symmetric_vectors[1])==FALSE & is.na(symmetric_vectors[2])==FALSE ),]
-Origin_Vector <- as.matrix(symmetric_vectors[2])
-Destination_Vector <- as.matrix(symmetric_vectors[1])
-for (i in 1:nrow(symmetric_vectors)){
-regexpression <- as.character(Origin_Vector[i])
-matches <- grep(regexpression, SOURCEVAR, perl = TRUE, ignore.case = TRUE, value=FALSE)
-for (j in matches){
-TARGET[j] <- as.character(Destination_Vector[i])
+`countrycode`<-function (SOURCEVAR, ORIGIN, DESTINATION) 
+{
+   	if (ORIGIN == "country.name") {
+		TARGET<-NA
+		for (i in 1:nrow(countrycode_data)) {
+            Origin_code <- as.character(countrycode_data$regex[i])
+			Destination_code<-as.matrix(countrycode_data[DESTINATION])
+            matches <- grep(Origin_code, SOURCEVAR, perl = TRUE, ignore.case = TRUE, value = FALSE)
+            for (j in matches) {
+            	TARGET[j] <- as.character(Destination_code[i])
+			}
+        }
+		TARGET
+	}
+    else {
+	TARGET<-NA
+        matchingmatrix <- countrycode_data[c(ORIGIN, DESTINATION)]
+        matchingmatrix <- matchingmatrix[which(is.na(matchingmatrix[ORIGIN]) == FALSE & is.na(matchingmatrix[DESTINATION]) == FALSE), ]
+        Origin_code <- as.matrix(matchingmatrix[ORIGIN])
+        Destination_code <- as.matrix(matchingmatrix[DESTINATION])
+        matches <- match(SOURCEVAR, Origin_code)
+        for (i in 1:length(SOURCEVAR)) {
+            ifelse(is.na(matches[i]), NA, TARGET[i] <- Destination_code[matches[i]])
+        }
+	TARGET
+    }
 }
-TARGET 
-}
-}else{
-symmetric_vectors <- countrycode_data[c(ORIGIN, DESTINATION)]
-symmetric_vectors <- symmetric_vectors[which (is.na(symmetric_vectors[ORIGIN])==FALSE & is.na(symmetric_vectors[DESTINATION])==FALSE ),]
-Origin_Vector <- as.matrix(symmetric_vectors[ORIGIN])
-Destination_Vector <- as.matrix(symmetric_vectors[DESTINATION])
-temp1 <- match(SOURCEVAR, Origin_Vector)
-for (i in 1:length(SOURCEVAR)){
-ifelse(is.na(temp1[i]), NA, TARGET[i] <- Destination_Vector[temp1[i]])
-}
-TARGET
-}
-}
-
