6192d53d0b810cf85826c709f8cc4a13f40acf8f
Vincent Arel-Bundock <varel@umich.edu>
2010-03-02 00:00:00 +0000

    version 0.4

--- R/countrycode.R
+++ R/countrycode.R
@@ -1,27 +1,35 @@
-`countrycode`<-function (SOURCEVAR, ORIGIN, DESTINATION) 
+countrycode<-
+function (SOURCEVAR, ORIGIN, DESTINATION) 
 {
-   	if (ORIGIN == "country.name") {
-		TARGET<-NA
-		for (i in 1:nrow(countrycode_data)) {
-            Origin_code <- as.character(countrycode_data$regex[i])
-			Destination_code<-as.matrix(countrycode_data[DESTINATION])
-            matches <- grep(Origin_code, SOURCEVAR, perl = TRUE, ignore.case = TRUE, value = FALSE)
-            for (j in matches) {
-            	TARGET[j] <- as.character(Destination_code[i])
+	if (ORIGIN == "country.name") {
+		# Prepare target
+		TARGET<-NULL
+		for (z in 1:length(SOURCEVAR)){TARGET<-c(NA,TARGET)}
+		# For each regex in the database -> find matches
+		for (i in 1:nrow(countrycode_data)){
+			Origin_code <- countrycode_data$regex[i]
+			Destination_code <- countrycode_data[i,DESTINATION]
+			matches <- as.vector(grep(Origin_code, as.vector(SOURCEVAR), perl = TRUE, ignore.case = TRUE, value = FALSE))
+			# For each match -> replace in target vector
+			for (j in matches) {
+				TARGET[j] <- Destination_code
 			}
-        }
+		}
+		TARGET
+		
+	}
+	else {
+		TARGET <- NA
+		matchingmatrix <- countrycode_data[c(ORIGIN, DESTINATION)]
+		matchingmatrix <- matchingmatrix[which(is.na(matchingmatrix[ORIGIN]) == 
+								FALSE & is.na(matchingmatrix[DESTINATION]) == FALSE), 
+		]
+		Origin_code <- as.matrix(matchingmatrix[ORIGIN])
+		Destination_code <- as.matrix(matchingmatrix[DESTINATION])
+		matches <- match(SOURCEVAR, Origin_code)
+		for (i in 1:length(SOURCEVAR)) {
+			ifelse(is.na(matches[i]), NA, TARGET[i] <- Destination_code[matches[i]])
+		}
 		TARGET
 	}
-    else {
-	TARGET<-NA
-        matchingmatrix <- countrycode_data[c(ORIGIN, DESTINATION)]
-        matchingmatrix <- matchingmatrix[which(is.na(matchingmatrix[ORIGIN]) == FALSE & is.na(matchingmatrix[DESTINATION]) == FALSE), ]
-        Origin_code <- as.matrix(matchingmatrix[ORIGIN])
-        Destination_code <- as.matrix(matchingmatrix[DESTINATION])
-        matches <- match(SOURCEVAR, Origin_code)
-        for (i in 1:length(SOURCEVAR)) {
-            ifelse(is.na(matches[i]), NA, TARGET[i] <- Destination_code[matches[i]])
-        }
-	TARGET
-    }
 }
