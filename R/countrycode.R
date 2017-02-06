72b1278f7ab5055a1f10924bc95b3ecd652115a4
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2017-02-06 20:36:27 +0000

    version 0.19

--- R/countrycode.R
+++ R/countrycode.R
@@ -5,64 +5,188 @@
 #' scheme to the official short English country name. Creates a new variable
 #' with the name of the continent or region to which each country belongs.
 #'
-#' @param sourcevar Vector which contains the codes or country names to be converted
-#' @param origin Coding scheme of origin (name enclosed in quotes "")
-#' @param destination Coding scheme of destination (name enclosed in quotes "")
+#' @param sourcevar Vector which contains the codes or country names to be
+#' converted (character or factor)
+#' @param origin Coding scheme of origin (string enclosed in quotes ""):
+#' "cowc", "cown", "eurostat", "fao", "fips105", "imf", "ioc", "iso2c", "iso3c",
+#' "iso3n", "p4_ccode", "p4_scode", "un", "wb", "wb_api2c", "wb_api3c", "wvs",
+#' "country.name", "country.name.de".
+#' @param destination Coding scheme of destination (string enclosed in quotes ""):
+#' "ar5", "continent", "cowc", "cown", "eurostat", "eu28", "eurocontrol_pru",
+#' "eurocontrol_statfor", "fao", "fips105", "icao", "icao_region", "imf",
+#' "ioc", "iso2c", "iso3c", "iso3n", "p4_ccode", "p4_scode", "region", "un",
+#' "wb", "wb_api2c", "wb_api3c", "wvs", "country.name", "country.name.ar",
+#' "country.name.de", "country.name.en", "country.name.es", "country.name.fr",
+#' "country.name.ru", "country.name.zh".
 #' @param warn Prints unique elements from sourcevar for which no match was found
+#' @param custom_dict A data frame which supplies custom country codes.
+#' Variables correspond to country codes, observations must refer to unique
+#' countries.  When countrycode uses a user-supplied dictionary, no sanity
+#' checks are conducted. The data frame format must resemble
+#' countrycode::countrycode_data.
+#' @param custom_match A named vector which supplies custom origin and
+#' destination matches that will supercede any matching default result. The name
+#' of each element will be used as the origin code, and the value of each
+#' element will be used as the destination code.
+#' @param origin_regex Logical: When using a custom dictionary, if TRUE then the
+#' origin codes will be matched as regex, if FALSE they will be matched exactly.
+#' When using the default dictionary (dictionary = NULL), origin_regex will be ignored.
 #' @keywords countrycode
-#' @note Supports the following coding schemes: Correlates of War character,
-#'   CoW-numeric, ISO3-character, ISO3-numeric, ISO2-character, IMF numeric, International
-#'   Olympic Committee, FIPS 10-4, FAO numeric, United Nations numeric,
-#'   World Bank character, official English short country names (ISO), continent, region.
-#'
-#'   The following strings can be used as arguments for \code{origin} or
-#'   \code{destination}: "cowc", "cown", "iso3c", "iso3n", "iso2c", "imf",
-#'   "fips104", "fao", "ioc", "un", "wb", "country.name".  The following strings can be
-#'   used as arguments for \code{destination} \emph{only}:  "continent", "region"
+#' @note For a complete description of available country codes and languages,
+#' please read the documentation for the \code{countrycode_data} conversion
+#' dictionary.  Type: \code{?countrycode_data}.
 #' @export
 #' @aliases countrycode
 #' @examples
-#' codes.of.origin <- countrycode::countrycode_data$cowc # Vector of values to be converted
-#' countrycode(codes.of.origin, "cowc", "iso3c")
-countrycode <- function (sourcevar, origin, destination, warn=FALSE){
-    # Sanity check
-    origin_codes <- names(countrycode::countrycode_data)[!(names(countrycode::countrycode_data) %in% c("continent","region","regex"))]
-    destination_codes <- names(countrycode::countrycode_data)[!(names(countrycode::countrycode_data) %in% c("regex"))]
-    if (!origin %in% origin_codes){stop("Origin code not supported")}
-    if (!destination %in% destination_codes){stop("Destination code not supported")}
-    if (origin == 'country.name'){
-        dict = na.omit(countrycode::countrycode_data[,c('regex', destination)])
-    }else{
-        dict = na.omit(countrycode::countrycode_data[,c(origin, destination)])
-    }
-    # Prepare output vector
-    destination_vector <- rep(NA, length(sourcevar))
-    # All but regex-based operations
-    if (origin != "country.name"){
-        matches <- match(sourcevar, dict[, origin])
-        destination_vector <- dict[matches, destination]
-    }else{
-        # For each regex in the database -> find matches
-        destination_list <- lapply(sourcevar, function(k) k)
-        for (i in 1:nrow(dict)){
-            matches <- grep(dict$regex[i], sourcevar, perl=TRUE, ignore.case=TRUE, value=FALSE)
-            destination_vector[matches] <- dict[i, destination]
-            # Warning-related
-            destination_list[matches] <- lapply(destination_list[matches], function(k) c(k, dict[i, destination]))
+#' # ISO to Correlates of War
+#' countrycode(c('USA', 'DZA'), 'iso3c', 'cown')
+#' # English to ISO
+#' countrycode('Albania', 'country.name', 'iso3c')
+#' # German to French
+#' countrycode('Albanien', 'country.name.de', 'country.name.fr')
+countrycode <- function(sourcevar, origin, destination, warn = TRUE,
+                        custom_dict = NULL, custom_match = NULL, origin_regex = FALSE) {
+    dictionary <- custom_dict
+
+    # Case-insensitive matching
+    if(is.null(dictionary)){ # only for built-in dictionary
+        if((class(sourcevar) == 'character') & !grepl('country', origin)){
+            sourcevar = toupper(sourcevar)
         }
-        destination_list <- destination_list[lapply(destination_list, length) > 2]
     }
+
+    # Regex defaults to English
+    if(origin == 'country.name'){
+        origin = 'country.name.en'
+    }
+    if(destination == 'country.name'){
+        destination = 'country.name.en'
+    }
+
+    # Auto-set origin_regex for regex origins
+    default_regex_codes <- c('country.name.en', 'country.name.de')
+    if (is.null(dictionary)) { # don't apply to custom dictionaries
+        if (origin %in% default_regex_codes) {
+            origin <- paste0(origin, '.regex')
+            origin_regex <- TRUE
+        } else {
+            origin_regex <- FALSE
+        }
+    }
+
+    # Sanity checks
+    if (missing(sourcevar)) {
+        stop('sourcevar is NULL (does not exist).')
+    }
+    if (! mode(sourcevar) %in% c('character', 'numeric')) {
+        stop('sourcevar must be a character or numeric vector. This error often
+             arises when users pass a tibble (e.g., from dplyr) instead of a
+             column vector from a data.frame (i.e., my_tbl[, 2] vs. my_df[, 2]
+                                              vs. my_tbl[[2]])')
+    }
+    if(is.null(dictionary)){ # built-in dictionary
+        if(is.null(sourcevar)){
+            stop("sourcevar is NULL (does not exist).", call. = FALSE)
+        }
+        bad_origin = c("ar5", "continent", "eurocontrol_pru",
+                       "eurocontrol_statfor", "eu28", "icao", "icao_region",
+                       "region", "country.name.ar", "country.name.es",
+                       "country.name.fr", "country.name.ru", "country.name.zh")
+        bad_destination = c('country.name.en.regex', 'country.name.de.regex')
+        if ((origin %in% bad_origin) | (!origin %in% colnames(countrycode::countrycode_data))){
+            stop("Origin code not supported")
+        }
+        if ((destination %in% bad_destination) | (!destination %in% colnames(countrycode::countrycode_data))){
+            stop("Destination code not supported")
+        }
+        dictionary = countrycode::countrycode_data
+    }else{ # custom dictionary
+        if('tbl_df' %in% class(dictionary)){ # allow tibble
+            dictionary <- as.data.frame(dictionary)
+        }
+        if(class(dictionary) != 'data.frame'){
+            stop("Custom dictionary must be a data frame or tibble with codes as columns.")
+        }
+        if(!origin %in% colnames(dictionary)){
+            stop("Origin code must correpond to a column name in the dictionary data frame.")
+        }
+        if(!destination %in% colnames(dictionary)){
+            stop("Destination code must correpond to a column name in the dictionary data frame.")
+        }
+        dups = any(duplicated(stats::na.omit(dictionary[, origin])))
+        if(dups){
+            stop("Countrycode cannot accept dictionaries with duplicated origin codes")
+        }
+    }
+
+    # Convert
+    if (origin_regex) { # regex codes
+        dict <- stats::na.omit(dictionary[, c(origin, destination)])
+        sourcefctr <- factor(sourcevar)
+
+        # match levels of sourcefctr
+        matches <-
+          sapply(c(levels(sourcefctr), NA), function(x) { # add NA so there's at least one item
+            matchidx <- sapply(dict[[origin]], function(y) grepl(y, x, perl = TRUE, ignore.case = TRUE))
+            dict[matchidx, destination]
+          })
+
+        # fill elements that have zero matches with the appropriate NA
+        matches[sapply(matches, length) == 0] <- `class<-`(NA, class(dict[[destination]]))
+
+        # create destination_list with elements that have more than one match
+        destination_list <- matches[sapply(matches, length) > 1]
+
+        # add sourcevar value to beginning of match results to replicate previous behavior
+        destination_list <- Map(c, names(destination_list), destination_list)
+
+        # set elements with multiple matches to the appropriate NA
+        matches[sapply(matches, length) > 1] <- `class<-`(NA, class(dict[[destination]]))
+
+        # remove all but last match to replicate previous behavior
+        matches <- sapply(matches, function(x) { x[length(x)] })
+
+        # replace with custom matches if set
+        if (!is.null(custom_match)) {
+          matchidxs <- match(names(matches), names(custom_match))
+          cust_matched <- !is.na(matchidxs)
+          matches[cust_matched] <- custom_match[matchidxs][cust_matched]
+        }
+
+        # apply new levels to sourcefctr and unname
+        destination_vector <- unname(matches[as.numeric(sourcefctr)])
+
+    } else { # non-regex codes
+        dict <- stats::na.omit(dictionary[, c(origin, destination)])
+        sourcefctr <- factor(sourcevar)
+
+        # match levels of sourcefctr
+        matchidxs <- match(levels(sourcefctr), dict[[origin]])
+        matches <- dict[[destination]][matchidxs]
+
+        # replace with custom matches if set
+        if (!is.null(custom_match)) {
+          matchidxs <- match(levels(sourcefctr), names(custom_match))
+          cust_matched <- !is.na(matchidxs)
+          matches[cust_matched] <- custom_match[matchidxs][cust_matched]
+        }
+
+        # apply new levels to sourcefctr
+        destination_vector <- matches[as.numeric(sourcefctr)]
+    }
+
     # Warnings
     if(warn){
         nomatch <- sort(unique(sourcevar[is.na(destination_vector)]))
+        nomatch <- nomatch[!nomatch %in% names(custom_match)]  # do not report <NA>'s that were set explicitly by custom_match
         if(length(nomatch) > 0){
-            warning("Some values were not matched: ", paste(nomatch, collapse=", "), "\n")
+            warning("Some values were not matched unambiguously: ", paste(nomatch, collapse=", "), "\n")
         }
-        if(origin=='country.name'){
+        if(origin_regex){
            if(length(destination_list) > 0){
                destination_list <- lapply(destination_list, function(k) paste(k, collapse=','))
                destination_list <- sort(unique(do.call('c', destination_list)))
-               warning("Some strings were matched more than once: ", paste(destination_list, collapse="; "), "\n")
+               warning("Some strings were matched more than once, and therefore set to <NA> in the result: ", paste(destination_list, collapse="; "), "\n")
            }
         }
     }
--- R/countrycode_data.R
+++ R/countrycode_data.R
@@ -1,25 +1,45 @@
 #' Country Code Translation Data Frame
 #'
-#' A data frame with 262 observations on the following 15 variables.
-#' Used internally by the \code{countrycode()} function.
+#' A data frame used internally by the \code{countrycode()} function.
 #'
 #' \itemize{
-#'   \item country.name: Long country name
+#'   \item ar5: IPCC's regional mapping used both in the Fifth Assessment Report
+#'              (AR5) and for the Reference Concentration Pathways (RCP)
+#'   \item continent: Continent as defined in the World Bank Development Indicators
 #'   \item cowc: Correlates of War character
 #'   \item cown: Correlates of War numeric
+#'   \item eurocontrol_pru:  European Organisation for the Safety of Air Navigation
+#'   \item eurocontrol_statfor:  European Organisation for the Safety of Air Navigation
+#'   \item eurostat:  Eurostat
+#'   \item eu28: Member states of the European Union (as of December 2015),
+#'               without special territories
 #'   \item fao: Food and Agriculture Organization of the United Nations numerical code
 #'   \item fips104: FIPS 10-4 (Federal Information Processing Standard)
+#'   \item icao: International Civil Aviation Organization
+#'   \item icao_region: International Civil Aviation Organization (Region)
 #'   \item imf: International Monetary Fund
+#'   \item ioc: International Olympic Committee
 #'   \item iso2c: ISO-2 character
 #'   \item iso3c: ISO-3 character
 #'   \item iso2n: ISO-2 numeric
 #'   \item iso3n: ISO-3 numeric
+#'   \item p4_ccode: Polity IV numeric country code
+#'   \item p4_scode: Polity IV alpha country code
+#'   \item region: Regions as defined in the World Bank Development Indicators
 #'   \item un: United Nations numerical code
 #'   \item wb: World Bank (very similar but not identical to iso3c)
-#'   \item ioc: International Olympic Committee
-#'   \item regex: Regular expression used to convert long names to code
-#'   \item region: Regions as defined in the World Bank Development Indicators
-#'   \item continent: Continent as defined in the World Bank Development Indicators
+#'   \item wb_api2c: World Bank API 2 character code
+#'   \item wb_api3c: World Bank API 3 character code
+#'   \item wvs: World Values Survey numeric code
+#'   \item country.name: country name (English)
+#'   \item country.name.en.regex: Regular expression used to convert English names to code
+#'   \item country.name.ar: country name (Arabic)
+#'   \item country.name.de: country name (German)
+#'   \item country.name.de.regex: Regular expression used to convert German names to code
+#'   \item country.name.es: country name (Spanish)
+#'   \item country.name.fr: country name (French)
+#'   \item country.name.ru: country name (Russian)
+#'   \item country.name.zh: country name (Chinese)
 #'
 #' }
 #'
@@ -46,6 +66,6 @@
 #' @keywords datasets
 #' @name countrycode_data
 #' @usage countrycode_data
-#' @format A data frame with 260 rows and 11 columns
+#' @format A data frame with 260 rows and 17 columns
 NULL
 
